7bf2b3b5595f2f81f272609f4b568ee5f9f02192
Mehdi Dehghani <dehghani.m.c@gmail.com>
2018-04-26 14:44:42 +0430

    Square Picasso v2.71828

---
+++ binding/RequestCreator.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Square.Picasso
+{
+    partial class RequestCreator
+    {
+        public virtual void Into(ImageView imageView, Action onSuccess, Action onError)
+        {
+            Into(imageView, new ActionCallback(onSuccess, onError));
+        }
+
+        private class ActionCallback : Java.Lang.Object, ICallback
+        {
+            private readonly Action onSuccess;
+            private readonly Action onError;
+
+            public ActionCallback(Action onSuccess, Action onError)
+            {
+                this.onSuccess = onSuccess;
+                this.onError = onError;
+            }
+            public void OnSuccess()
+            {
+                if (onSuccess != null)
+                {
+                    onSuccess();
+                }
+            }
+
+            public void OnError(Java.Lang.Exception p0)
+            {
+                if (onError != null)
+                {
+                    onError();
+                }
+            }
+        }
+    }
+}
